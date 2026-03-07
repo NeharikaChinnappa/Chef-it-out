fe2ffa6e4083ce28285f7ae9ce94aa2e0b0b009d
Palecanda Neharika Chinnappa <70146693+NeharikaChinnappa@users.noreply.github.com>
2026-03-07 21:20:34 +0530

    Add files via upload
    made some changes --- final for review 2

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
-const restaurantRoutes = require("./routes/restaurantRoutes");
-const reportRoutes = require("./routes/reportRoutes");
-const reviewRoutes = require("./routes/reviewRoutes");
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
+app.use("/uploads", express.static("uploads"));
+
+const authRoutes = require("./routes/authRoutes");
+const restaurantRoutes = require("./routes/restaurantRoutes");
+const reportRoutes = require("./routes/reportRoutes");
+const uploadProofRoutes = require("./routes/uploadProof");
+
+app.use("/auth", authRoutes);
 app.use("/restaurants", restaurantRoutes);
 app.use("/reports", reportRoutes);
-app.use("/reviews", reviewRoutes);
+app.use("/upload-proof", uploadProofRoutes);
 
-app.listen(5000, () => {
- console.log("Server running on port 5000");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
